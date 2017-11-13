af27508510d0655464aa3f175ed00550c5a6c92f
Gary <tp841025@gmail.com>
2017-11-14 00:20:58 +0800

    updateTarget

--- updateTarget.php
+++ updateTarget.php
@@ -15,12 +15,9 @@
 	$targetContent=$_POST['targetContent'];
 	$targetStartTime=$_POST['targetStartTime'];
 	$targetEndTime=$_POST['targetEndTime'];
-	$state=$_POST['state'];
-	$auth=$_POST['auth'];
-	$dream=$_POST['dream'];
 	
 	
-	$sql="UPDATE ".$table." SET targetName='$targetName' , targetContent='$targetContent' , targetStartTime='$targetStartTime' , targetEndTime='$targetEndTime' , state='$state' , auth='$auth' , dream='$dream' WHERE tid='$tid'";
+	$sql="UPDATE ".$table." SET targetName='$targetName' , targetContent='$targetContent' , targetStartTime='$targetStartTime' , targetEndTime='$targetEndTime' WHERE tid='$tid'";
 	$result = $dbgo->prepare($sql); 
 	echo $result->execute(); 
 ?>
