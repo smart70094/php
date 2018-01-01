806463e654e3357564a3d539d77029551a4f57af
Jim <b10323024@yuntech.edu.tw>
2018-01-01 13:54:56 +0800

    merge 結案

---
+++ updateStateForYTarget.php
@@ -0,0 +1,16 @@
+<?php
+	$db_host = "localhost";
+	$db_user = "root";
+	$db_pass = "";
+	$db_select = "teamgogoal";
+	$dbconnect = "mysql:host=".$db_host.";dbname=".$db_select;
+	$dbgo = new PDO($dbconnect, $db_user, $db_pass);
+	$dbgo->query('set character set utf8');
+	
+	$tid=$_POST['tid'];
+	$sql="UPDATE target SET state='Y' WHERE tid='$tid'";
+	echo $dbgo->exec($sql); 
+?>
+
+
+
